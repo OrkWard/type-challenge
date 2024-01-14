024cf7c9672e7801ae66760142e68ebcbad3ff94
OrkWard <orkward_o@outlook.com>
2024-01-15 00:56:52 +0800

    feat(11): easy

---
+++ 11-tuple-to-object.ts
@@ -0,0 +1,10 @@
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+
+type TupleToObject<T extends readonly string[]> = {
+  [U in T[number]]: U;
+};
+
+type result = TupleToObject<typeof tuple>;
+// expected { 'tesla': 'tesla', 'model 3': 'model 3', 'model X': 'model X', 'model Y': 'model Y'}
+
+export {};
