cbf297e8573d6a7607238756f90997563aaf92ad
OrkWard <orkward_o@outlook.com>
2024-01-25 00:03:17 +0800

    feat(898): fix for test case 15

--- 898-includes.ts
+++ 898-includes.ts
@@ -2,12 +2,14 @@ import type { Equal, Expect } from "@type-challenges/utils";
 
 /* _____________ Your Code Here _____________ */
 
-type Includes<T extends readonly any[], U> = Equal<T[0], U> extends true
-  ? true
-  : T extends [infer First, ...infer Rest]
-  ? Includes<Rest, U>
+type Includes<T extends readonly any[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
   : false;
-
 /* _____________ Test Cases _____________ */
 
 type cases = [
